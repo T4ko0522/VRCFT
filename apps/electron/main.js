e0e8b49507c0ef572f3bb62a8794d2d7ea7893b9
Tako <tako.renraku1@gmail.com>
2025-04-18 18:40:45 +0900

    feat: enable external links to open in the default browser

--- apps/electron/main.js
+++ apps/electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, shell } = require('electron');
 const path = require('path');
 
 function createWindow() {
@@ -12,6 +12,23 @@ function createWindow() {
   });
 
   win.loadURL('http://localhost:3000');
+
+  // 外部リンクをデフォルトのブラウザで開く
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    if (url.startsWith('http')) {
+      shell.openExternal(url);
+      return { action: 'deny' };
+    }
+    return { action: 'allow' };
+  });
+
+  win.webContents.on('will-navigate', (event, url) => {
+    const currentUrl = win.webContents.getURL();
+    if (url !== currentUrl && url.startsWith('http')) {
+      event.preventDefault();
+      shell.openExternal(url);
+    }
+  });
 }
 
 app.whenReady().then(() => {
